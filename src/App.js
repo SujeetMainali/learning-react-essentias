9f3387b4c98d23cd1aed78ec075ec8aaed652ccd
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-18 16:13:07 +0545

    passing arrays to keep record the history of state changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 
 function App() {
   const [emotion, setEmotion]  = useState('happy');
+  const [secondary, setSecondary] = useState('tired');
 
   useEffect( () =>{
     console.log(`the current emotion is ${emotion}`)
-  })
+  }, [emotion]);
+
+  useEffect (()=>{
+    console.log(`the current secondary emotion is ${secondary}`)
+  }, [secondary])
   return (
     <div className="App">
       
@@ -17,6 +22,8 @@ function App() {
       </button>
 
       <button onClick={()=>{setEmotion('excited')}}>excited</button>
+      <h2>current secondary emotion is {secondary}</h2>
+      <button onClick={()=>setSecondary('i dont know')}>Dont know</button>
        
     </div>
   );
