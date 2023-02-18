9dd9c41c8043bed3032641bfde33a57f4fc37062
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-18 16:34:48 +0545

    incorporating useReducer hook to change the state of checkbox

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 
-import { useEffect, useState } from 'react';
+import {  useReducer } from 'react';
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion]  = useState('happy');
-  const [secondary, setSecondary] = useState('tired');
+  // const [emotion, setEmotion]  = useState('happy');
+  // const [secondary, setSecondary] = useState('tired');
 
-  useEffect( () =>{
-    console.log(`the current emotion is ${emotion}`)
-  }, [emotion]);
+  // useEffect( () =>{
+  //   console.log(`the current emotion is ${emotion}`)
+  // }, [emotion]);
 
-  useEffect (()=>{
-    console.log(`the current secondary emotion is ${secondary}`)
-  }, [secondary])
+  // useEffect (()=>{
+  //   console.log(`the current secondary emotion is ${secondary}`)
+  // }, [secondary])
+  const [checked, setChecked] = useReducer((checked)=>!checked,false);
   return (
     <div className="App">
+        <input type= 'checkbox' value={checked} onChange = {setChecked
+        } />
+          <label>{checked ? "Checked" : "UnChecked"}</label>
+        
+
+
       
-        <h1>Current emotion is {emotion}</h1>
+        {/* <h1>Current emotion is {emotion}</h1>
       <button onClick={()=>{setEmotion('sad')}}>
         sad
       </button>
 
       <button onClick={()=>{setEmotion('excited')}}>excited</button>
       <h2>current secondary emotion is {secondary}</h2>
-      <button onClick={()=>setSecondary('i dont know')}>Dont know</button>
+      <button onClick={()=>setSecondary('i dont know')}>Dont know</button> */}
        
     </div>
   );
