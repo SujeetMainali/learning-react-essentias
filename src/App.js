ef94488e3497796ed6e602a9c63c975f368b2d92
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-19 23:41:45 +0545

    added notes for better understanding fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-
-import {  useReducer } from 'react';
 import './App.css';
+import {  useState, useEffect } from 'react';
 
-function App() {
-  // const [emotion, setEmotion]  = useState('happy');
-  // const [secondary, setSecondary] = useState('tired');
-
-  // useEffect( () =>{
-  //   console.log(`the current emotion is ${emotion}`)
-  // }, [emotion]);
-
-  // useEffect (()=>{
-  //   console.log(`the current secondary emotion is ${secondary}`)
-  // }, [secondary])
-  const [checked, setChecked] = useReducer((checked)=>!checked,false);
-  // useReducer takes two parameters, one is function to change the state and another is the initial state 
+function App () {
+  const [data, setData] = useState(null);
+  useEffect (()=>{
+    fetch(
+      'https://api.github.com/users/sujeetmainali'
+    ).then((response)=> response.json())
+    .then(setData)
+  },[])
+  if(data)
+    return(
+      <pre>{JSON.stringify(data, null, 10)}</pre>
+      //data is the data and null, 10 is the formatting of the json to make it readable
+    )
   return (
     <div className="App">
-        <input type= 'checkbox' value={checked} onChange = {setChecked
-        } />
-          <label>{checked ? "Checked" : "UnChecked"}</label>
-        
-
-
-      
-        {/* <h1>Current emotion is {emotion}</h1>
-      <button onClick={()=>{setEmotion('sad')}}>
-        sad
-      </button>
-
-      <button onClick={()=>{setEmotion('excited')}}>excited</button>
-      <h2>current secondary emotion is {secondary}</h2>
-      <button onClick={()=>setSecondary('i dont know')}>Dont know</button> */}
+        <h1>data</h1>
        
     </div>
-  );
+  )
 }
+
 
 export default App;
